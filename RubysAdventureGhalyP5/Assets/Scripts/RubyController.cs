55ea9acd950e47ea5e355257ca83a4f817a4824c
voidklll <716229@student.dusd.net>
2023-11-06 13:49:41 -0800

    Added Prefabs
    Added lots of prefabs decorated map added no rotation to ruby and  fixed code

--- RubysAdventureGhalyP5/Assets/Scripts/RubyController.cs
+++ RubysAdventureGhalyP5/Assets/Scripts/RubyController.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent < Rigidbody2D();
+
     }
 
     // Update is called once per frame
@@ -14,9 +21,13 @@ public class RubyController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+
+        rigidbody2d.MovePosition(position);
     }
 }
