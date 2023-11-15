32588fa487a084fad685bde9d39c26456e2ab881
voidklll <716229@student.dusd.net>
2023-11-15 13:44:06 -0800

    Finished Blocking Movment and Collecetables
    Finished 3 videos

--- RubysAdventureGhalyP5/Assets/Scripts/HealthCollectable.cs
+++ RubysAdventureGhalyP5/Assets/Scripts/HealthCollectable.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthCollectable : MonoBehaviour
+public class HealthCollectible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
+        RubyController controller = other.GetComponent<RubyController>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (controller != null)
+        {
+            if (controller.health < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- RubysAdventureGhalyP5/Assets/Scripts/RubyController.cs
+++ RubysAdventureGhalyP5/Assets/Scripts/RubyController.cs
@@ -8,7 +8,9 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
+
     int currentHealth;
+    public int health { get { return currentHealth; } }
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -17,9 +19,8 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent < Rigidbody2D >();
+        rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -28,19 +29,20 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
     }
+
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
-
 }
+
