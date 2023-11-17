28c45e2060ffcb924f55680c7b3598dd2c9a8532
voidklll <716229@student.dusd.net>
2023-11-17 13:44:48 -0800

    Finished 2 Videos
    World Collectables, Damage Zones and enemys.

--- RubysAdventureGhalyP5/Assets/Scripts/RubyController.cs
+++ RubysAdventureGhalyP5/Assets/Scripts/RubyController.cs
@@ -8,9 +8,13 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
 
-    int currentHealth;
     public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -28,6 +32,13 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     void FixedUpdate()
@@ -41,8 +52,15 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+        }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
-
